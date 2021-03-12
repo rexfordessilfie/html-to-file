f22672ca0134935619265e130f5e7ac432abef9f
Rexford Essilfie <rexfordessilfie09@gmail.com>
2021-03-12 15:47:37 -0600

    Add singleton-ish version of PuppeteerFileGenerator

--- util/generators.ts
+++ util/generators.ts
@@ -1,10 +1,13 @@
 import * as puppeteer from "puppeteer";
+import { ensureFileExtension } from "./helpers";
 
 interface ImageOptions {
   selector?: string;
   height?: number;
   width?: number;
 }
+
+interface PdfOptions {}
 
 export const extractImageOptions = (obj: any) => {
   const { selector, width, height } = obj;
@@ -14,6 +17,19 @@ export const extractImageOptions = (obj: any) => {
 export interface HtmlToFileGenerator {
   generateImage(options?: any): string | Promise<string>;
   generatePdf(options?: any): string | Promise<string>;
+}
+
+export interface HtmlToFileGeneratorSingleton {
+  generateImage(
+    url: string,
+    destination: string,
+    options?: ImageOptions
+  ): Promise<string>;
+  generatePdf(
+    url: string,
+    destination: string,
+    options?: PdfOptions
+  ): Promise<string>;
 }
 
 export class PuppeteerGenerator implements HtmlToFileGenerator {
@@ -39,8 +55,9 @@ export class PuppeteerGenerator implements HtmlToFileGenerator {
     await this.page.goto(this.url, { waitUntil: "networkidle0" });
   }
 
-  async tearDown() {
-    await this.browser.close();
+  static async tearDown() {
+    await PuppeteerGeneratorSingleton.browser?.close();
+    PuppeteerGeneratorSingleton.browser = null
   }
 
   async generateImage(options: any): Promise<string> {
@@ -54,7 +71,7 @@ export class PuppeteerGenerator implements HtmlToFileGenerator {
         path: `${this.fileLocation}/${fileNamePlusExtension}`,
         ...screenshotOptions,
       });
-      await this.tearDown();
+      await PuppeteerGeneratorSingleton.tearDown();
       console.log("[PuppeteerGenerator] Done generating image");
       return fileNamePlusExtension;
     } catch (error) {
@@ -71,7 +88,7 @@ export class PuppeteerGenerator implements HtmlToFileGenerator {
         path: `${this.fileLocation}/${fileNamePlusExtension}`,
         ...options,
       });
-      await this.tearDown();
+      await PuppeteerGeneratorSingleton.tearDown();
       console.log("[PuppeteerGenerator] Done generating pdf.");
       return fileNamePlusExtension;
     } catch (error) {
@@ -108,5 +125,107 @@ export class PuppeteerGenerator implements HtmlToFileGenerator {
       };
     });
     await this.page.setViewport(newViewport);
+  }
+}
+
+export class PuppeteerGeneratorSingleton
+  implements HtmlToFileGeneratorSingleton {
+  static browser: puppeteer.Browser | null;
+  static _instance: PuppeteerGeneratorSingleton;
+  
+  page = {} as puppeteer.Page | undefined
+
+  constructor() {
+    if (!PuppeteerGeneratorSingleton.browser) {
+      PuppeteerGeneratorSingleton.setUp();
+      PuppeteerGeneratorSingleton._instance = this;
+    }
+    return PuppeteerGeneratorSingleton._instance
+  }
+
+  static async setUp() {
+    PuppeteerGeneratorSingleton.browser = await puppeteer.launch({
+      // should allow puppeteer to work in heroku
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+  }
+
+  static async tearDown() {
+    await PuppeteerGeneratorSingleton.browser?.close();
+    PuppeteerGeneratorSingleton.browser = null
+  }
+
+  async loadBrowserPage (url: string){
+    this.page = await PuppeteerGeneratorSingleton.browser?.newPage();
+    await this.page?.goto(url, { waitUntil: "networkidle0" }); // Make sure content has finished loading on page
+  }
+
+  async generateImage(url:string, filename:string, options: any): Promise<string> {
+    console.log("[PuppeteerGenerator] About to generate image...");
+    const fileWithExtension = ensureFileExtension(filename, '.png')
+    try {
+      await this.loadBrowserPage(url);
+      const processedOptions = await this.processImageOptions(options);
+      const { target, ...screenshotOptions } = processedOptions;
+      await target?.screenshot({
+        path: fileWithExtension,
+        ...screenshotOptions,
+      });
+      console.log("[PuppeteerGenerator] Done generating image");
+      return fileWithExtension;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async generatePdf(url:string, filename:string, options: any): Promise<string> {
+    console.log("[PuppeteerGenerator] About to generate pdf...");
+    const fileWithExtension = ensureFileExtension(filename, '.png')
+    try {
+      await this.loadBrowserPage(url)
+      await this.page?.pdf({
+        path: ensureFileExtension(filename, '.pdf'),
+        ...options,
+      });
+      console.log("[PuppeteerGenerator] Done generating pdf.");
+      return fileWithExtension;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Page must already be active before handling options
+  async processImageOptions(options: ImageOptions): Promise<any> {
+    console.log("[PuppeteerGenerator] Processing image options...");
+    const { selector, width, height } = options;
+    let processedOptions = {};
+
+    if (!this.page){
+      return {}
+    }
+
+    const target = selector ? await this.page?.$(selector) : this.page;
+    processedOptions = { ...processedOptions, target };
+
+    if (width) {
+      processedOptions = { ...processedOptions, width };
+    }
+    if (height) {
+      processedOptions = { ...processedOptions, height };
+    }
+    return processedOptions;
+  }
+
+  async resizePage() {
+    // https://petertran.com.au/2018/07/12/blank-images-puppeteer-screenshots-solved/
+    // Resize the viewport to screenshot elements outside of the viewport
+    const newViewport = await this.page?.$eval("body", (bodyHandle) => {
+      const boundingBox = bodyHandle.getBoundingClientRect();
+      return {
+        width: Math.ceil(boundingBox.width),
+        height: Math.ceil(boundingBox.height),
+      };
+    }) as puppeteer.Viewport;
+    await this.page?.setViewport(newViewport);
   }
 }
