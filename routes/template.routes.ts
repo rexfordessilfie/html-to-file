0b9395368342d55c15b80010e0d17b0e64be686a
Rexford Essilfie <rexfordessilfie09@gmail.com>
2022-01-13 02:28:47 -0500

    chore: bring views to top level

--- routes/template.routes.ts
+++ routes/template.routes.ts
@@ -7,7 +7,7 @@ router.get("/:name", (req, res) => {
   const templateName = req.params.name as string;
   const { fallbackUrl = "" } = req.query;
   const data = { fallbackUrl, baseUrl: PUBLIC_URL };
-  res.render(`../templates/${templateName}`, data);
+  res.render(`${templateName}`, data);
 });
 
 export { router };
